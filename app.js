8dbb4c20d4c2b98289aabe474e8d7b6ebc7f4aec
Ales Bacul <alesbacul@gmail.com>
2024-03-21 17:43:29 +0100

    app.js cleaning

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
-const ejs = require("ejs");
-const methodOverride = require("method-override")
-const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-const { middleware } = require("./controllers/user");
 const path = require("path");
+const { middleware } = require("./controllers/user");
 
+// Config
+const PORT = process.env.PORT || 3000;
+
+const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://127.0.0.1:27017/tnpw_ales")
   .catch((error) => console.error("Chyba při připojení k databázi", error));
 
 const db = mongoose.connection;
-
 db.once("open", () => {
   console.log("Databáze je připojená");
 });
 
 // Utils
+app.disable('x-powered-by');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(middleware);
-app.use(methodOverride('_method'))
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
